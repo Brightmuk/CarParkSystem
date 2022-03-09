16476cdeec985d0d36be5b48680279b008cf2f97
brimuk <be.rightmuk@gmail.com>
2022-03-09 22:58:25 +0300

    Create home form

---
+++ HomeForm.Designer.vb
@@ -0,0 +1,443 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class HomeForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnInit = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnAmmend = New System.Windows.Forms.Button()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.lblNumberPlate = New System.Windows.Forms.Label()
+        Me.lblOwner = New System.Windows.Forms.Label()
+        Me.lblParkingSpot = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.FileSystemWatcher1 = New System.IO.FileSystemWatcher()
+        Me.btnFirst = New System.Windows.Forms.Button()
+        Me.btnPrevious = New System.Windows.Forms.Button()
+        Me.btnNext = New System.Windows.Forms.Button()
+        Me.btnLast = New System.Windows.Forms.Button()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lblTitleOne = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.comboParkingType = New System.Windows.Forms.ComboBox()
+        Me.lblType = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.comboSpot = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtFee = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtNumberPlate = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtOwner = New System.Windows.Forms.TextBox()
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnInit
+        '
+        Me.btnInit.Location = New System.Drawing.Point(724, 355)
+        Me.btnInit.Name = "btnInit"
+        Me.btnInit.Size = New System.Drawing.Size(75, 23)
+        Me.btnInit.TabIndex = 0
+        Me.btnInit.Text = "Init"
+        Me.btnInit.UseVisualStyleBackColor = True
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(221, 375)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(118, 25)
+        Me.btnAdd.TabIndex = 1
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(221, 256)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(118, 25)
+        Me.btnUpdate.TabIndex = 2
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(221, 299)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(118, 27)
+        Me.btnDelete.TabIndex = 3
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnAmmend
+        '
+        Me.btnAmmend.Location = New System.Drawing.Point(58, 299)
+        Me.btnAmmend.Name = "btnAmmend"
+        Me.btnAmmend.Size = New System.Drawing.Size(118, 27)
+        Me.btnAmmend.TabIndex = 4
+        Me.btnAmmend.Text = "Ammend"
+        Me.btnAmmend.UseVisualStyleBackColor = True
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(58, 374)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(118, 27)
+        Me.btnSearch.TabIndex = 5
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'lblNumberPlate
+        '
+        Me.lblNumberPlate.AutoSize = True
+        Me.lblNumberPlate.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblNumberPlate.Location = New System.Drawing.Point(67, 62)
+        Me.lblNumberPlate.Name = "lblNumberPlate"
+        Me.lblNumberPlate.Size = New System.Drawing.Size(97, 14)
+        Me.lblNumberPlate.TabIndex = 6
+        Me.lblNumberPlate.Text = "Number Plate"
+        '
+        'lblOwner
+        '
+        Me.lblOwner.AutoSize = True
+        Me.lblOwner.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblOwner.Location = New System.Drawing.Point(67, 159)
+        Me.lblOwner.Name = "lblOwner"
+        Me.lblOwner.Size = New System.Drawing.Size(52, 14)
+        Me.lblOwner.TabIndex = 7
+        Me.lblOwner.Text = "Owner"
+        '
+        'lblParkingSpot
+        '
+        Me.lblParkingSpot.AutoSize = True
+        Me.lblParkingSpot.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblParkingSpot.Location = New System.Drawing.Point(67, 109)
+        Me.lblParkingSpot.Name = "lblParkingSpot"
+        Me.lblParkingSpot.Size = New System.Drawing.Size(92, 14)
+        Me.lblParkingSpot.TabIndex = 9
+        Me.lblParkingSpot.Text = "Parking Spot"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(239, 156)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox1.TabIndex = 11
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(239, 59)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox2.TabIndex = 12
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(239, 106)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(100, 23)
+        Me.ComboBox1.TabIndex = 14
+        '
+        'FileSystemWatcher1
+        '
+        Me.FileSystemWatcher1.EnableRaisingEvents = True
+        Me.FileSystemWatcher1.SynchronizingObject = Me
+        '
+        'btnFirst
+        '
+        Me.btnFirst.Location = New System.Drawing.Point(58, 209)
+        Me.btnFirst.Name = "btnFirst"
+        Me.btnFirst.Size = New System.Drawing.Size(60, 23)
+        Me.btnFirst.TabIndex = 15
+        Me.btnFirst.Text = "First"
+        Me.btnFirst.UseVisualStyleBackColor = True
+        '
+        'btnPrevious
+        '
+        Me.btnPrevious.Location = New System.Drawing.Point(124, 209)
+        Me.btnPrevious.Name = "btnPrevious"
+        Me.btnPrevious.Size = New System.Drawing.Size(75, 23)
+        Me.btnPrevious.TabIndex = 16
+        Me.btnPrevious.Text = "Previous"
+        Me.btnPrevious.UseVisualStyleBackColor = True
+        '
+        'btnNext
+        '
+        Me.btnNext.Location = New System.Drawing.Point(205, 209)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(66, 23)
+        Me.btnNext.TabIndex = 17
+        Me.btnNext.Text = "Next"
+        Me.btnNext.UseVisualStyleBackColor = True
+        '
+        'btnLast
+        '
+        Me.btnLast.Location = New System.Drawing.Point(277, 209)
+        Me.btnLast.Name = "btnLast"
+        Me.btnLast.Size = New System.Drawing.Size(62, 23)
+        Me.btnLast.TabIndex = 18
+        Me.btnLast.Text = "Last"
+        Me.btnLast.UseVisualStyleBackColor = True
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(58, 256)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(118, 27)
+        Me.btnDisplay.TabIndex = 19
+        Me.btnDisplay.Text = "Display"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.lblTitleOne)
+        Me.Panel1.Controls.Add(Me.btnUpdate)
+        Me.Panel1.Controls.Add(Me.btnLast)
+        Me.Panel1.Controls.Add(Me.btnDisplay)
+        Me.Panel1.Controls.Add(Me.btnNext)
+        Me.Panel1.Controls.Add(Me.btnPrevious)
+        Me.Panel1.Controls.Add(Me.btnAdd)
+        Me.Panel1.Controls.Add(Me.btnFirst)
+        Me.Panel1.Controls.Add(Me.lblNumberPlate)
+        Me.Panel1.Controls.Add(Me.ComboBox1)
+        Me.Panel1.Controls.Add(Me.btnAmmend)
+        Me.Panel1.Controls.Add(Me.btnDelete)
+        Me.Panel1.Controls.Add(Me.lblOwner)
+        Me.Panel1.Controls.Add(Me.lblParkingSpot)
+        Me.Panel1.Controls.Add(Me.TextBox2)
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Controls.Add(Me.btnSearch)
+        Me.Panel1.Location = New System.Drawing.Point(0, 2)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(433, 446)
+        Me.Panel1.TabIndex = 20
+        '
+        'lblTitleOne
+        '
+        Me.lblTitleOne.AutoSize = True
+        Me.lblTitleOne.Font = New System.Drawing.Font("Verdana", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblTitleOne.Location = New System.Drawing.Point(149, 7)
+        Me.lblTitleOne.Name = "lblTitleOne"
+        Me.lblTitleOne.Size = New System.Drawing.Size(98, 25)
+        Me.lblTitleOne.TabIndex = 28
+        Me.lblTitleOne.Text = "Actions"
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Panel2.Controls.Add(Me.lblTitle)
+        Me.Panel2.Controls.Add(Me.DateTimePicker1)
+        Me.Panel2.Controls.Add(Me.Label5)
+        Me.Panel2.Controls.Add(Me.comboParkingType)
+        Me.Panel2.Controls.Add(Me.lblType)
+        Me.Panel2.Controls.Add(Me.Label1)
+        Me.Panel2.Controls.Add(Me.comboSpot)
+        Me.Panel2.Controls.Add(Me.Label2)
+        Me.Panel2.Controls.Add(Me.txtFee)
+        Me.Panel2.Controls.Add(Me.Label3)
+        Me.Panel2.Controls.Add(Me.txtNumberPlate)
+        Me.Panel2.Controls.Add(Me.Label4)
+        Me.Panel2.Controls.Add(Me.txtOwner)
+        Me.Panel2.Location = New System.Drawing.Point(430, 2)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(439, 446)
+        Me.Panel2.TabIndex = 20
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Verdana", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblTitle.Location = New System.Drawing.Point(167, 7)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(98, 25)
+        Me.lblTitle.TabIndex = 27
+        Me.lblTitle.Text = "Display"
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(228, 338)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(100, 23)
+        Me.DateTimePicker1.TabIndex = 26
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label5.Location = New System.Drawing.Point(56, 344)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(38, 14)
+        Me.Label5.TabIndex = 25
+        Me.Label5.Text = "Date"
+        '
+        'comboParkingType
+        '
+        Me.comboParkingType.FormattingEnabled = True
+        Me.comboParkingType.Location = New System.Drawing.Point(228, 177)
+        Me.comboParkingType.Name = "comboParkingType"
+        Me.comboParkingType.Size = New System.Drawing.Size(100, 23)
+        Me.comboParkingType.TabIndex = 24
+        '
+        'lblType
+        '
+        Me.lblType.AutoSize = True
+        Me.lblType.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblType.Location = New System.Drawing.Point(56, 180)
+        Me.lblType.Name = "lblType"
+        Me.lblType.Size = New System.Drawing.Size(94, 14)
+        Me.lblType.TabIndex = 23
+        Me.lblType.Text = "Parking Type"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(56, 68)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(97, 14)
+        Me.Label1.TabIndex = 15
+        Me.Label1.Text = "Number Plate"
+        '
+        'comboSpot
+        '
+        Me.comboSpot.FormattingEnabled = True
+        Me.comboSpot.Location = New System.Drawing.Point(228, 121)
+        Me.comboSpot.Name = "comboSpot"
+        Me.comboSpot.Size = New System.Drawing.Size(100, 23)
+        Me.comboSpot.TabIndex = 22
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(56, 237)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 14)
+        Me.Label2.TabIndex = 16
+        Me.Label2.Text = "Owner"
+        '
+        'txtFee
+        '
+        Me.txtFee.Location = New System.Drawing.Point(228, 290)
+        Me.txtFee.Name = "txtFee"
+        Me.txtFee.Size = New System.Drawing.Size(100, 23)
+        Me.txtFee.TabIndex = 21
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(56, 124)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(92, 14)
+        Me.Label3.TabIndex = 17
+        Me.Label3.Text = "Parking Spot"
+        '
+        'txtNumberPlate
+        '
+        Me.txtNumberPlate.Location = New System.Drawing.Point(228, 65)
+        Me.txtNumberPlate.Name = "txtNumberPlate"
+        Me.txtNumberPlate.Size = New System.Drawing.Size(100, 23)
+        Me.txtNumberPlate.TabIndex = 20
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(56, 290)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(31, 14)
+        Me.Label4.TabIndex = 18
+        Me.Label4.Text = "Fee"
+        '
+        'txtOwner
+        '
+        Me.txtOwner.Location = New System.Drawing.Point(228, 234)
+        Me.txtOwner.Name = "txtOwner"
+        Me.txtOwner.Size = New System.Drawing.Size(100, 23)
+        Me.txtOwner.TabIndex = 19
+        '
+        'HomeForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(870, 450)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.btnInit)
+        Me.Name = "HomeForm"
+        Me.Text = "Form1"
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnInit As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnAmmend As Button
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents lblNumberPlate As Label
+    Friend WithEvents lblOwner As Label
+    Friend WithEvents lblParkingSpot As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents FileSystemWatcher1 As IO.FileSystemWatcher
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents Label5 As Label
+    Friend WithEvents comboParkingType As ComboBox
+    Friend WithEvents lblType As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents comboSpot As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtFee As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtNumberPlate As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtOwner As TextBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents lblTitleOne As Label
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents btnLast As Button
+    Friend WithEvents btnNext As Button
+    Friend WithEvents btnPrevious As Button
+    Friend WithEvents btnFirst As Button
+End Class
---
+++ HomeForm.vb
@@ -0,0 +1,23 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class HomeForm
+    Dim conn As New MySqlConnection
+
+    Private Sub btnInit_Click(sender As Object, e As EventArgs) Handles btnInit.Click
+        Dim databaseName As String = "mysqlconnector"
+        Dim server As String = "localhost"
+        Dim username As String = "root"
+        Dim password As String = "beatsbydre"
+        If Not conn Is Nothing Then conn.Close()
+        conn.ConnectionString = "server=" & server & ";user id=" & username & ";password=" & password & ";database=" & databaseName & ""
+
+        Try
+            conn.Open()
+            MessageBox.Show("Mysql connected!")
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+End Class
